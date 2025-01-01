308b440b95a2bd0b2fd31f15d8e9c4ebd6b08c4c
Denis Nasonov <brain0991@gmail.com>
2025-01-02 00:32:30 +0300

    fix results

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("\'arr\' parameter must be an instance of the Array!");
+  }
+  if (arr.length === 0) {
+    return [];
+  }
+  let arrCopy = Array.from(arr);
+  let result = [];
+ 
+  if (arrCopy[0] == '--discard-prev' || arrCopy[0] == '--double-prev') {
+    result = arrCopy.slice(1);
+    return result
+  }
+  if (arrCopy[arrCopy.length-1] == '--discard-next' || arrCopy[arrCopy.length-1] == '--double-next') {
+    result = arrCopy.slice(0, -1);
+    return result
+  }
+    for (let i = 0; i < arrCopy.length; i++) {
+      if (arrCopy[i] == '--discard-next' && arrCopy[i + 2] == '--double-prev') {
+        result = arrCopy.slice(0, i);
+        result = result.concat(arrCopy.slice(i + 3));
+        return result;
+      }
+      if (arrCopy[i] == '--double-next') {
+        result.push(arrCopy[i+1]);
+      } else if (arrCopy[i] == '--discard-next') {
+        i += 1;
+      } else if (arrCopy[i] == '--double-prev') {
+        result.push(arrCopy[i - 1]);
+      } else if (arrCopy[i] == '--discard-prev') {
+        result.splice(i - 1, 1);
+      }
+      else {
+        if (arrCopy[i] !== undefined) {
+          result.push(arrCopy[i]);
+        }
+      }
+  }
+
+  return result
 }
 
 module.exports = {
